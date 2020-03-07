69e8bc914b08901bcbbdcc3c182384401eddfb55
JonathanWac <jonathanwachholz@gmail.com>
2020-03-07 01:30:36 -0600

    Maeven pom.xml file added

--- src/main/java/TOKENS.java
+++ src/main/java/TOKENS.java
@@ -1,5 +1,5 @@
 public class TOKENS {
-    static private String OpenWeatherKEY = "ENTER YOUR API KEY HERE";
+    static private String OpenWeatherKEY = "04f06bba65bfe41c793c01e99b83b5a2";
 
     public static String getOpenWeatherKEY() {
         return OpenWeatherKEY;
