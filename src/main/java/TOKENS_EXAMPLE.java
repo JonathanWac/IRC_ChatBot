dfa7d5db2bab8866e8a8a45eb359217acc62d89f
JonathanWac <jonathanwachholz@gmail.com>
2020-03-09 03:32:17 -0500

    Add Tokens_Examples.java

---
+++ src/main/java/TOKENS_EXAMPLE.java
@@ -0,0 +1,19 @@
+
+
+public class TOKENS_EXAMPLE {
+    //This file is for GitHub documentation purposes only
+    //  Anyone downloading this repo needs to:
+    //      1) Rename this file to TOKENS.java
+    //      2) Change the Class name to TOKENS
+    //          3)Go to both https://openweathermap.org/
+    //          4)And https://developer.mapquest.com/documentation/
+    //              Sign up for a free account and insert your API Keys into the Quotes below
+    //
+    //
+
+    public static String getOpenWeatherAPIKEY() { return  "INSERT_OpenWeatherAPIKeyHere"; }
+
+    public static String getMapQuestAPIKEY(){ return "INSERT_MapQuestAPIKeyHere";}
+
+    private static String getMediaWikiAPIKEY(){return "NO API KEY NEEDED AT THIS TIME";} // Just here for future use
+}
