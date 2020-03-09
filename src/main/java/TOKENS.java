f922c1c5e27e95478ae7963a6d7b16870becb049
JonathanWac <jonathanwachholz@gmail.com>
2020-03-09 03:53:50 -0500

    Add Tokens_Examples.java

--- src/main/java/TOKENS.java
+++ src/main/java/TOKENS.java
@@ -3,4 +3,6 @@ public class TOKENS {
     public static String getOpenWeatherAPIKEY() { return  "04f06bba65bfe41c793c01e99b83b5a2"; }
 
     public static String getMapQuestAPIKEY(){ return "ddQR7mPWuL6gnFxdU5FiiDEMoobaFpBQ";}
+
+    private static String getMediaWikiAPIKEY(){return "NO API KEY NEEDED AT THIS TIME";} // Just here for future use
 }
